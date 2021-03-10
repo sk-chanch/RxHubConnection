eba3d2663808339752eee61ebb198535658f0acf
Khuad <khuad.chanchana@megazy.com>
2021-03-10 17:36:05 +0700

    -

---
+++ Example/Pods/Target Support Files/Pods-RxHubConnection_Tests/Pods-RxHubConnection_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RxHubConnection_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_RxHubConnection_Tests
+@end
---
+++ Example/Pods/Target Support Files/RxHubConnection/RxHubConnection-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_RxHubConnection : NSObject
+@end
+@implementation PodsDummy_RxHubConnection
+@end
