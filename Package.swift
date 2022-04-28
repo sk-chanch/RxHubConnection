b804d44da9d4cf92d87d7e18d0b8f6e87f85bdcc
skipp <chanchanakoedtho@gmail.com>
2022-04-28 21:27:16 +0700

    Fixed pakcage

--- Package.swift
+++ Package.swift
@@ -6,7 +6,6 @@ import PackageDescription
 let package = Package(
     name: "RxHubConnection",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "RxHubConnection",
             targets: ["RxHubConnection"]),
@@ -18,13 +17,12 @@ let package = Package(
        
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "RxHubConnection",
-            dependencies: ["RxSwift",
-                .product(name: "RxCocoa", package: "RxSwift"),
-                .product(name: "SignalRClient", package: "SignalR-Client-Swift")
+            dependencies: [
+                .product(name: "SignalRClient", package: "SignalR-Client-Swift"),
+                .product(name: "RxSwift", package: "RxSwift"),
+                .product(name: "RxCocoa", package: "RxSwift")
             ],
             path: "RxHubConnection/Classes")
     ]
--- RxHubConnection/Classes/RxHubConnection.swift
+++ RxHubConnection/Classes/RxHubConnection.swift
@@ -10,7 +10,7 @@
 import Foundation
 import RxSwift
 import RxCocoa
-import SwiftSignalRClient
+import SignalRClient
 
 extension HubConnection: HasDelegate {
     public typealias Delegate = HubConnectionDelegate
