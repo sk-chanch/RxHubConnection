9a35ed56125a9a1aa79b6d96e4857c8419483ba0
skipp <chanchanakoedtho@gmail.com>
2022-04-28 20:56:30 +0700

    Fixed dependencies

--- Package.swift
+++ Package.swift
@@ -19,7 +19,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "RxHubConnection",
-            dependencies: ["RxSwift", "RxCocoa"],
+            dependencies: ["RxSwift",
+                .product(name: "RxCocoa", package: "RxSwift")],
             path: "RxHubConnection/Classes")
     ]
 )
