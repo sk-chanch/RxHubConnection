7903edf828152837fe59d1516ea621f854d9418d
skipp <chanchanakoedtho@gmail.com>
2022-04-28 20:53:52 +0700

    Add swift package

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "RxHubConnection",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "RxHubConnection",
+            targets: ["RxHubConnection"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMinor(from: "6.5.0")),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "RxHubConnection",
+            dependencies: ["RxSwift", "RxCocoa"],
+            path: "RxHubConnection/Classes")
+    ]
+)
